0e6a5e930cd4f627848fcc0be5a39af82b383476
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-06-06 20:22:49 -0400

    Update logic in temperature file

--- temperature.py
+++ temperature.py
@@ -1,4 +1,30 @@
+def validate_units(frm, to):
+    units = ["f", "fahrenheit", "c", "celsius", "k", "kelvin"]
+    frm = frm.lower()
+    to = to.lower()
+    return (frm in units) and (to in units)
+
+
+def abbreviate(unit):
+    result = ""
+    match unit:
+        case "fahrenheit":
+            result = "f"
+        case "celsius":
+            result = "c"
+        case "kelvin":
+            result = "k"
+        case _:
+            result = unit
+    return result
+
+
 def convert_temperature(num, frm, to):
+    valid_units = validate_units(frm, to)
+    if not valid_units:
+        raise ValueError
+    frm = abbreviate(frm)
+    to = abbreviate(to)
     result = 0
     if frm == "f":
         if to == "c":
@@ -7,8 +33,6 @@ def convert_temperature(num, frm, to):
             result = ((num - 32) * 5 / 9) + 273.15
         elif to == "f":
             result = num
-        else:
-            print("Invalid unit.")
     elif frm == "c":
         if to == "f":
             result = (num * 9 / 5) + 32 # c to f 
@@ -16,8 +40,6 @@ def convert_temperature(num, frm, to):
             result = num + 273.15
         elif to == "c":
             result = num
-        else:
-            print("Invalid unit.")
     elif frm == "k":
         if to == "f":
             result = (num - 273.15) * 9 / 5 + 32
@@ -25,8 +47,4 @@ def convert_temperature(num, frm, to):
             result = num - 273.15
         elif to == "k":
             result = num
-        else:
-            print("Invalid unit. Returning 0...")
-    else:
-        print("Invalid unit")
     return result
